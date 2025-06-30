2cb32424e485a64a0c6aa3e77d1e907ba540bf43
Diya Punetha <diyapunetha333@gmail.com>
2025-06-30 20:28:18 +0530

    30-06-2025

--- Primary Department for each employee.sql
+++ Primary Department for each employee.sql
@@ -53,13 +53,8 @@ Explanation:
 - The Primary department for employee 3 is 3.
 - The Primary department for employee 4 is 3. */
 
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y'
-
-UNION
-
-SELECT employee_id, department_id
-FROM Employee
-GROUP BY employee_id
-HAVING COUNT(*) = 1;
+select employee_id, department_id from Employee
+where primary_flag = 'Y' or
+employee_id in
+(select employee_id from Employee group by employee_id
+having count(employee_id) = 1)
