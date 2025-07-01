0f3bb87d5b3171ece69f42002f3b4c4643f9078a
Diya Punetha <diyapunetha333@gmail.com>
2025-07-01 23:39:33 +0530

    1-07-2025

--- Consecutive NUmbers.sql
+++ Consecutive NUmbers.sql
@@ -41,8 +41,7 @@ Output:
 +-----------------+
 Explanation: 1 is the only number that appears consecutively for at least three times. */
 
-SELECT DISTINCT l1.num AS ConsecutiveNums
-FROM Logs l1
-JOIN Logs l2 ON l1.id = l2.id - 1
-JOIN Logs l3 ON l1.id = l3.id - 2
-WHERE l1.num = l2.num AND l2.num = l3.num;
+select distinct t.num ConsecutiveNums 
+from (select num, lead(num,1) OVER (ORDER BY id) as next,
+lag(num,1) OVER (ORDER BY id) as prev from logs) t 
+where t.num=t.prev and t.num=t.next;
